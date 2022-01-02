1a87e002f23d03c8b2b61dbdd847b4e1c740f92c
TakeruTomii <dev.haunted.house@gmail.com>
2022-01-02 19:52:39 +0900

    modify allow cor origins from enviromental values

--- setting.py
+++ setting.py
@@ -1,10 +1,7 @@
 import os
 FLASK_SECRET_KEY = os.environ['FLASK_SECRET_KEY']
 
-ALLOW_COR_ORIGINS = [
-    "http://localhost:4200",
-    "https://prd-haunted-house.herokuapp.com"
-]
+ALLOW_COR_ORIGINS = os.environ['ALLOW_COR_ORIGINS']
 
 USERS = {os.environ['MAIL_USER_1']:os.environ['MAIL_PASS_1']}
 
